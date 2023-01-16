e73e5fb39cedfe0ec68647d854cffd6cda04b300
Paulo Vitor <pv.marques2010@hotmail.com>
2023-01-16 15:28:08 -0300

    Logo: remove

--- src/assets/components/header/Header.tsx
+++ src/assets/components/header/Header.tsx
@@ -3,7 +3,6 @@
 export const Header = () => {
     return(
         <header className="header">
-            <img src="./public/img/ignite-logo.svg" alt="Logo Ignite" />
             <h1>Ignite Feed</h1>
         </header>
     )
